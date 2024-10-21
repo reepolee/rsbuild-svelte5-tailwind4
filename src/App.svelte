47efe21f9c929f384ebedd33a9ca7aeca7a758c1
Aleš Vaupotič <ales@vaupotic.eu>
2024-10-21 09:38:46 +0200

    Tailwind 4 alpha on Svelte 5

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Hello from './components/Hello.svelte';
+	let { name } = $props();
+</script>
+
+<main class="grid h-svh place-items-center">
+	<div class="flex flex-col gap-y-4">
+		<a href="https://www.reepolee.com">
+			<img src="reepolee.svg" alt="Reepolee logo" class="h-12" />
+		</a>
+		<Hello {name}></Hello>
+		<p>Ready for your next big project?</p>
+	</div>
+</main>
---
+++ src/components/Hello.svelte
@@ -0,0 +1,11 @@
+<script>
+	let { name } = $props();
+</script>
+
+<h1>Hello {name}!</h1>
+
+<style>
+	h1 {
+		font-size: var(--font-size-3xl);
+	}
+</style>
