14ccf55b719cc891a06fe24c4df4794b05ededa9
Aleš Vaupotič <ales@vaupotic.eu>
2024-10-21 09:52:38 +0200

    grid content

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,10 @@
 	let { name } = $props();
 </script>
 
-<main class="grid h-svh place-items-center">
-	<div class="flex flex-col gap-y-4">
-		<a href="https://www.reepolee.com">
-			<img src="reepolee.svg" alt="Reepolee logo" class="h-12" />
-		</a>
-		<Hello {name}></Hello>
-		<p>Ready for your next big project?</p>
-	</div>
+<main class="grid h-svh place-content-center gap-y-4">
+	<a href="https://www.reepolee.com">
+		<img src="reepolee.svg" alt="Reepolee logo" class="h-12" />
+	</a>
+	<Hello {name}></Hello>
+	<p>Ready for your next big project?</p>
 </main>
